42babee5277ad2eb4db47334a0938d7ad397d3e0
lucascostamr <lucascostamr812@gmail.com>
2024-05-05 12:44:31 -0300

    test: ensure AddClientController returns 400 if no name is provided

---
+++ src/test/java/com/packetdelivery/clientservice/presentation/controller/AddClientControllerTests.java
@@ -0,0 +1,33 @@
+package com.packetdelivery.clientservice.presentation.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+import com.packetdelivery.clientservice.presentation.controller.AddClientController;
+import com.packetdelivery.clientservice.protocols.HttpRes;
+import com.packetdelivery.clientservice.protocols.HttpReq;
+import com.packetdelivery.clientservice.model.domain.AddClientModel;
+
+public class AddClientControllerTests {
+    @Getter
+    @AllArgsConstructor
+    class FakeAddClient implements AddClientModel{
+        private String name;
+        private String email;
+        private String cnpj;
+        private String phone;
+    }
+    
+    @Test
+    void return_400_if_no_name_is_provided(){
+        AddClientController sut = new AddClientController();
+        FakeAddClient fakeAddClient = new FakeAddClient(null, "any_email", "any_cnpj", "any_phone");
+        HttpReq fakeRequest = new HttpReq(fakeAddClient);
+        HttpRes response = sut.handle(fakeRequest);
+        assertEquals(response.getStatusCode(), 400);
+    }
+}
