8bd41d845a9c3abdcc089ab8fdbd2c069391a85d
lucascostamr <lucascostamr812@gmail.com>
2024-05-09 11:23:49 -0300

    refactor: add getEmail to IAddClientModel

---
+++ src/main/java/com/packetdelivery/clientservice/model/domain/IAddClientModel.java
@@ -0,0 +1,11 @@
+package com.packetdelivery.clientservice.model.domain;
+
+public interface IAddClientModel {
+    public String getName();
+
+    public String getEmail();
+
+    public String getCnpj();
+
+    public String getPhone();
+}
