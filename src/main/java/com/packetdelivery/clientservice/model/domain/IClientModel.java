9ebda7564d70fbf49394166b477a84449ae5bd7a
lucascostamr <lucascostamr812@gmail.com>
2024-05-08 22:44:55 -0300

    feat: ensure AddClientController calls AddClientRepository with correct values

---
+++ src/main/java/com/packetdelivery/clientservice/model/domain/IClientModel.java
@@ -0,0 +1,9 @@
+package com.packetdelivery.clientservice.model.domain;
+
+public interface IClientModel {
+    public String getId();
+    public String getName();
+    public String getEmail();
+    public String getCnpj();
+    public String getPhone();
+}
